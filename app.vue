f6b6fa00715c22bf573041bb33eed5dda562773d
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-02-10 21:22:40 +0100

    Update app.vue
    Тестируем VueFire

--- app.vue
+++ app.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 const { data } = await useFetch("/api/hello");
+const user = useCurrentUser();
+
+console.log(user);
 </script>
 
 <template>
   <div>
     <pre>{{ data }}</pre>
+    <pre>{{ user }}</pre>
   </div>
 </template>
